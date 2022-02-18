c3200f4cac36fb3b3845b4dee370f0635da3f0f4
Bolatti_Jona <jonathanbolatti@gmail.com>
2022-02-18 08:06:17 -0300

    Creation Testimonials Entity

--- src/main/java/com/alkemy/ong/entity/TestimonyEntity.java
+++ src/main/java/com/alkemy/ong/entity/TestimonyEntity.java
@@ -1,5 +1,42 @@
 package com.alkemy.ong.entity;
 
-public class Testimony {
-    
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "testimonials")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE testimonials SET deleted = true WHERE id=?")
+@Where(clause = "deleted = false")
+public class TestimonyEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "deleted", nullable = false)
+    private Boolean deleted = Boolean.FALSE;
+
+    @Column(name= "creationDate" ,nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
 }
