aabb0db5c0bcb5c61eaed0c1f1b2c31334177c9d
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-02-18 16:30:23 -0300

    created Member entity and repository

---
+++ src/main/java/com/alkemy/ong/entity/MemberEntity.java
@@ -0,0 +1,57 @@
+package com.alkemy.ong.entity;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Member")
+public class MemberEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull
+    @Column( name="name", length = 50, nullable = false)
+    private String name;
+
+    @Column( name="facebookUrl", length = 100)
+    private String facebookUrl;
+
+    @Column( name="instagramUrl", length = 100)
+    private String instagramUrl;
+
+    @Column( name="linkedinUrl", length = 100)
+    private String linkedinUrl;
+
+    @NotNull
+    @Column( name="image", length = 200, nullable = false)
+    private String image;
+
+    @Column( name="description", length = 250)
+    private String description;
+
+    @Column(name= "creationDate" ,nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
+    //softDelete
+    @NotNull
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted=Boolean.FALSE;
+
+}
