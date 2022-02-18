e354eb62ebb1f9895388345f1114a653fae42e32
Dante <danteiturri@gmail.com>
2022-02-18 15:16:56 -0300

    Create the Activity entity

---
+++ src/main/java/com/alkemy/ong/entity/ActivityEntity.java
@@ -0,0 +1,46 @@
+package com.alkemy.ong.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="activities")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE activities SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false ")
+public class ActivityEntity {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name= "uuid", strategy = "uuid2")
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @NotNull
+    @Column(nullable = false, length = 50)
+    private String name;
+    @NotNull
+    @Column(nullable = false, length = 150)
+    private String content;
+    @Column(nullable = false)
+    private String image;
+
+    private boolean deleted = Boolean.FALSE;
+
+    @Column(name= "creationDate" ,nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+
+
+
+}
+
