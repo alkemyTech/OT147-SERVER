9727f2420e2aeff222c08b3a637266f3f9a7392a
Pablo Frankenberger <pablo.frankenn@gmail.com>
2022-02-17 22:44:35 -0300

    Modifed LocalDate and uuid

--- src/main/java/com/alkemy/ong/entity/Organization.java
+++ src/main/java/com/alkemy/ong/entity/Organization.java
@@ -1,15 +1,20 @@
 package com.alkemy.ong.entity;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
 @Data
 @Table(name="organizations")
 @SQLDelete(sql = "UPDATE organizations SET deleted = true WHERE id=?")
+@NoArgsConstructor
+@Where(clause = "deleted=false")
 public class Organization {
     @Id
     @GeneratedValue(generator = "uuid")
@@ -25,23 +30,24 @@ public class Organization {
     private String address;
 
     @Column(name = "PHONE")
-    private int phone;
+    private String phone;
 
     @Column(name = "EMAIL", nullable = false)
     private String email;
 
-    @Column(name = "WELCOME_TEXT", nullable = false)
+
+    @Column(name = "WELCOME_TEXT", nullable = false, columnDefinition = "TEXT")
     private String welcomeText;
 
-    @Column(name = "ABOUT_US_TEXT")
+
+    @Column(name = "ABOUT_US_TEXT", columnDefinition = "TEXT")
     private String aboutUsText;
 
     private boolean deleted=Boolean.FALSE;
 
-    @Column(name= "creationDate" ,nullable = false, updatable = false,
-            columnDefinition="TIMESTAMP WITH DATE")
-    @Temporal(TemporalType.DATE)
-    private Date creationDate;
+    @Column(name= "creationDate" ,nullable = false,updatable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
 
     @Override
     public boolean equals(Object o) {
