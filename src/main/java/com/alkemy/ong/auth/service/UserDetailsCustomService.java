4112ac2d3b8d8910493d56f1a9dffcd66d05bd8b
walterio123 <walterio12345@gmail.com>
2022-02-21 16:01:48 -0300

    Security Configuration Update

--- src/main/java/com/alkemy/ong/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/ong/auth/service/UserDetailsCustomService.java
@@ -3,12 +3,18 @@ package com.alkemy.ong.auth.service;
 import com.alkemy.ong.entity.UserEntity;
 import com.alkemy.ong.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Transactional
 @Service
 public class UserDetailsCustomService implements  UserDetailsService {
 
@@ -36,7 +42,16 @@ public class UserDetailsCustomService implements  UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user=userRepository.findByEmail(email);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found in te db");
+        }
+        Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRoleId().getName()));
+
+        return new org.springframework.security.core.userdetails
+                .User( user.getEmail(), user.getPassword(), authorities);
     }
-}
+    }
+
