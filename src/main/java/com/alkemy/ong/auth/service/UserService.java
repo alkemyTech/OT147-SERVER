cfe1496689f7dda00baeea4d5d785278add997c4
Bolatti_Jona <jonathanbolatti@gmail.com>
2022-02-22 07:13:15 -0300

    Se elimino UserServiceRepo por que ya estaba creado como UserRepository

--- src/main/java/com/alkemy/ong/auth/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.alkemy.ong.auth.service;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserService {
-    UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException;
-}
