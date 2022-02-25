484099af813ec85d3e14deb51d0cd130531080c4
Dante <danteiturri@gmail.com>
2022-02-25 02:55:56 -0300

    add modified UserMapper Usercontroller Userservice IUserservice

--- src/main/java/com/alkemy/ong/service/UserService.java
+++ src/main/java/com/alkemy/ong/service/UserService.java
@@ -1,5 +1,11 @@
 package com.alkemy.ong.service;
 
+import com.alkemy.ong.auth.dto.UserDTO;
+import com.alkemy.ong.entity.UserEntity;
+
+
+
 public interface UserService {
       void deleteUserById(String id);
+      UserEntity updateUser(UserEntity entity) ;
 }
