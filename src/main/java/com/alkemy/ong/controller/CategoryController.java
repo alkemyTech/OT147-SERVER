d3b8d0d14dd88278e64cc9094e13f052bf40ee9c
Dante <danteiturri@gmail.com>
2022-02-25 15:04:05 -0300

    change name in the RequestMappig for word to lowercase

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Category")
+@RequestMapping("/category")
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
