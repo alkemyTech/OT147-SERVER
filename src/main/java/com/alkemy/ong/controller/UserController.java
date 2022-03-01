ca670d5f41935a727fc9a26b25bf84e369c2a6b2
anavduo <anavduo@hotmail.com>
2022-03-01 20:28:01 -0300

    Added Role Validation through HttpSecurity, deleted @PreAuthorize from UserController, updated NewsEntity and NewsController

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -6,8 +6,8 @@ import com.alkemy.ong.mapper.UserMapper;
 import com.alkemy.ong.repository.UserRepository;
 import com.alkemy.ong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,21 +29,22 @@ public class UserController {
     private UserRepository userRepository;
 
     //method to get a list of users
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @GetMapping("/list")
     public ResponseEntity<List<UserDTO>> UserList() {
         List<UserDTO> users = userService.getAllUser();
         return ResponseEntity.ok().body(users);
     }
 
-    //Method for hard delete
+    // Soft deletion method
     @DeleteMapping("/{id}")
     public ResponseEntity<UserEntity> delete(@PathVariable String id) {
-        this.userService.deleteUserById(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
+        try {
+            this.userService.deleteUserById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
-
     /*Method for actualization for id */
     @PatchMapping("/user/{id}")
     public ResponseEntity<UserDTO> update(@PathVariable String id) {
