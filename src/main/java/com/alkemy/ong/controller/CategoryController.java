631682be0d1e6be87fc5c5d9fcba3cec1f95b1a9
Dante <danteiturri@gmail.com>
2022-03-02 18:42:13 -0300

    two java.until.list

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController
