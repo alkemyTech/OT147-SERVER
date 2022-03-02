40134385af1c83d2ddff83cbeb792ee5de9bfbb0
walterio123 <walterio12345@gmail.com>
2022-03-02 09:28:54 -0300

    Update Category For ID

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -1,23 +1,27 @@
 package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.CategoryDto;
+import com.alkemy.ong.dto.CategoryDtoFull;
 import com.alkemy.ong.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
-
-    private final CategoryService categoryService;
+private final CategoryService categoryService;
 
     @GetMapping("/categories")
     public ResponseEntity <List<CategoryDto>> getAllCategories(){
         return ResponseEntity.ok(categoryService.getAll());
+    }
+    //Update information of category only for Admin User
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<CategoryDtoFull> update(@PathVariable String id, @RequestBody CategoryDtoFull categoryDtoFull){
+
+        return ResponseEntity.ok(categoryService.update(id,categoryDtoFull));
     }
 }
