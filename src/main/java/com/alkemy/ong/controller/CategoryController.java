32549b8744e06c361114bb02d4131c0109519607
Bolatti_Jona <jonathanbolatti@gmail.com>
2022-03-02 15:10:03 -0300

    Create PR OT147-42-BIS

--- src/main/java/com/alkemy/ong/controller/CategoryController.java
+++ src/main/java/com/alkemy/ong/controller/CategoryController.java
@@ -3,11 +3,11 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 @RequiredArgsConstructor
 @RestController
@@ -20,4 +20,12 @@ public class CategoryController {
     public ResponseEntity <List<CategoryDto>> getAllCategories(){
         return ResponseEntity.ok(categoryService.getAll());
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoryDto> addCategory(@Valid @RequestBody CategoryDto categoryDTO){
+        CategoryDto savedCategory = categoryService.addCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+
+    }
+
 }
--- src/main/java/com/alkemy/ong/service/CategoryService.java
+++ src/main/java/com/alkemy/ong/service/CategoryService.java
@@ -1,9 +1,11 @@
-package com.alkemy.ong.service.impl;
+package com.alkemy.ong.service;
 
 import com.alkemy.ong.dto.CategoryDto;
 import com.alkemy.ong.entity.CategoryEntity;
 import com.alkemy.ong.mapper.CategoryMapper;
+import com.alkemy.ong.mapper.CategoryMapperSimple;
 import com.alkemy.ong.repository.CategoryEntityRepository;
+import com.alkemy.ong.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,24 @@ public class CategoryService {
     private CategoryMapper categoryMapper;
     @Autowired
     private CategoryEntityRepository categoryEntityRepository;
+    @Autowired
+    CategoryMapperSimple categoryMapperSimple;
+    @Autowired
+    CategoryRepository categoryRepository;
 
     //Get all Category from Database.
     public List<CategoryDto> getAll(){
        List<CategoryEntity> categoryEntityList=categoryEntityRepository.findAll();
 
         return categoryMapper.listCategoryEntityToListCategoryDto(categoryEntityList);
+    }
+
+
+    public CategoryDto addCategory(CategoryDto dto) {
+        CategoryEntity entity = categoryMapperSimple.categoryDTOToCategoryEntity(dto);
+        CategoryEntity savedEntity =categoryRepository.save(entity);
+        CategoryDto result = categoryMapperSimple.categoryEntityToCategoryDTO(savedEntity);
+        return result;
     }
 
 }
