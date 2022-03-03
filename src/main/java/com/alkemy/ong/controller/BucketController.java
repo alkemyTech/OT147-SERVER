c1f29fb9c33f03e46959963e2d0f2aaf9df9244e
Lucas Franco <shananestudios@gmail.com>
2022-03-03 16:49:44 -0300

    I add the BucketController and change the properties again

---
+++ src/main/java/com/alkemy/ong/controller/BucketController.java
@@ -0,0 +1,26 @@
+package com.alkemy.ong.controller;
+
+import com.alkemy.ong.service.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage/")
+public class BucketController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+}
