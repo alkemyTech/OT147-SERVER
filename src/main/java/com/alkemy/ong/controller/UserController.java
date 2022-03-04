cc21f980c691ce6fc209d14557f1dd7795284456
anavduo <anavduo@hotmail.com>
2022-03-03 22:27:25 -0300

    Implemented UserMapper using MapStruct

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.alkemy.ong.controller;
 
-import com.alkemy.ong.dto.UserDTO;
+import com.alkemy.ong.dto.UserDto;
 import com.alkemy.ong.entity.UserEntity;
-import com.alkemy.ong.mapper.UserMapper;
-import com.alkemy.ong.repository.UserRepository;
 import com.alkemy.ong.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,36 +22,32 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
 
-    //method to get a list of users
-    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+    //Method to get a list of users
     @GetMapping("/list")
-    public ResponseEntity<List<UserDTO>> UserList() {
-        List<UserDTO> users = userService.getAllUser();
+    public ResponseEntity<List<UserDto>> UserList() {
+        List<UserDto> users = userService.getAllUser();
         return ResponseEntity.ok().body(users);
     }
 
     //Method for hard delete
     @DeleteMapping("/{id}")
     public ResponseEntity<UserEntity> delete(@PathVariable String id) {
+        try {
         this.userService.deleteUserById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
-    }
-
-    /*Method for actualization for id */
-    @PatchMapping("/user/{id}")
-    public ResponseEntity<UserDTO> update(@PathVariable String id) {
-        try {
-
-            UserEntity entity = userRepository.findById(id).get();
-            return new ResponseEntity<UserDTO>(UserMapper
-                    .updateUserEntityToDTO(userService.updateUser(entity)), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
 
+    /*Method for actualization for id */
+    @PatchMapping("/{id}")
+    public ResponseEntity<UserDto> update(@PathVariable String id) {
+        try {
+            return ResponseEntity.ok(userService.updateUser(id));
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
