1e534e59a26eda624cd178164791a4eb91b3bc86
Dante <danteiturri@gmail.com>
2022-03-07 20:23:37 -0300

    commit query custom and import

--- src/main/java/com/alkemy/ong/controller/OrganizationController.java
+++ src/main/java/com/alkemy/ong/controller/OrganizationController.java
@@ -2,12 +2,17 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.dto.OrganizationDto;
 
+import com.alkemy.ong.dto.SlideDtoFull;
 import com.alkemy.ong.service.OrganizationService;
+import com.alkemy.ong.service.SlideService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/organization")
