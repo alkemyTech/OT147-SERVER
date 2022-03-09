2bd628c614d5566c57c5c1b41849477d8a2303d6
anavduo <anavduo@hotmail.com>
2022-03-09 12:55:11 -0300

    Added endpoint to add Members to the DB for User

--- src/main/java/com/alkemy/ong/repository/MemberRepository.java
+++ src/main/java/com/alkemy/ong/repository/MemberRepository.java
@@ -2,10 +2,10 @@ package com.alkemy.ong.repository;
 
 import com.alkemy.ong.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface MemberRepository  extends JpaRepository <MemberEntity, String> {
-
-
-}
+   }
