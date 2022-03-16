a096f3c52fc9c80f908d3226abbcc24d76327397
Bolatti_Jona <jonathanbolatti@gmail.com>
2022-03-16 18:34:15 -0300

    I add documentation with swagger for the members entity.

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -3,11 +3,18 @@ package com.alkemy.ong.controller;
 import com.alkemy.ong.dto.MemberDto;
 import com.alkemy.ong.dto.PagesDto;
 import com.alkemy.ong.service.MemberService;
+import com.alkemy.ong.util.DocumentationMessages;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.List;
 
@@ -16,17 +23,35 @@ import static org.springframework.http.ResponseEntity.status;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/members")
+@Tag(name = "Members", description = "Create, update show and delete Members")
 public class MemberController {
+
     @Autowired
-    private  MemberService memberService;
+    private MemberService memberService;
+
     //Members creation method for User
-    @PostMapping
-    public ResponseEntity<MemberDto> addMember(@Valid @RequestBody MemberDto memberDto)  {
-            MemberDto savedMember = memberService.addMember(memberDto);
-            return status(HttpStatus.CREATED).body(savedMember);
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = DocumentationMessages.MEMBER_CONTROLLER_SUMMARY_CREATE)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_201_DESCRIPTION)})
+    @Tag(name = "Members")
+    public ResponseEntity<MemberDto> addMember(@Valid @RequestBody MemberDto memberDto) {
+        MemberDto savedMember = memberService.addMember(memberDto);
+        return status(HttpStatus.CREATED).body(savedMember);
     }
+
     //Delete member by id (soft delete)
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}")
+    @Operation(summary = DocumentationMessages.MEMBER_CONTROLLER_SUMMARY_DELETE)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_204_DESCRIPTION),
+            @ApiResponse(responseCode = "403",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_403_DESCRIPTION),
+            @ApiResponse(responseCode = "404",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_404_DESCRIPTION)})
+    @Tag(name = "Members")
     public ResponseEntity<Void> delete(@PathVariable String id) throws Exception {
         try {
             memberService.deleteMemberById(id);
@@ -35,22 +60,41 @@ public class MemberController {
         }
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-    //Method to get a list of members
+
     // Method to get a list of members with 10 members role User
-    @GetMapping()
-    public ResponseEntity<?> membersPageable(@RequestParam (required= false, defaultValue = "-1") int page) {
-        if(page!=-1) {
+    @Tag(name = "Members")
+    @GetMapping(params = "page")
+    @Operation(summary = DocumentationMessages.MEMBER_CONTROLLER_SUMMARY_LIST)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_200_DESCRIPTION),
+            @ApiResponse(responseCode = "400",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_400_DESCRIPTION),
+            @ApiResponse(responseCode = "403",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_403_DESCRIPTION)})
+    public ResponseEntity<?> membersPageable(@RequestParam(required = false, defaultValue = "-1") int page) {
+        if (page != -1) {
             PagesDto<MemberDto> response = memberService.searchPaginatedMembers(page);
             return ResponseEntity.ok().body(response);
         }
         List<MemberDto> response = memberService.getAllMembers();
         return ResponseEntity.ok().body(response);
     }
-    @PutMapping("/{id}")
-    public ResponseEntity<MemberDto> update(@PathVariable String id, @RequestBody MemberDto memberDto) {
+
+    // Method to update  of members
+    @Tag(name = "Members")
+    @PutMapping(value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = DocumentationMessages.MEMBER_CONTROLLER_SUMMARY_UPDATE)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_200_DESCRIPTION),
+            @ApiResponse(responseCode = "403",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_403_DESCRIPTION),
+            @ApiResponse(responseCode = "404",
+                    description = DocumentationMessages.MEMBER_CONTROLLER_RESPONSE_404_DESCRIPTION)})
+    public ResponseEntity<MemberDto> update(@PathVariable String id, @Valid @RequestBody MemberDto memberDto) {
         return ResponseEntity.ok(memberService.update(id, memberDto));
     }
 }
-
-
-
---
+++ src/main/java/com/alkemy/ong/util/DocumentationMessages.java
@@ -0,0 +1,108 @@
+package com.alkemy.ong.util;
+
+public class DocumentationMessages {
+    public static final String MEMBER_CONTROLLER = "Member Controller";
+
+    public static final String MEMBER_CONTROLLER_DESCRIPTION = "Contains the "
+            + "endpoints of member entity";
+
+    public static final String MEMBER_CONTROLLER_SUMMARY_LIST = "List of Members";
+
+    public static final String MEMBER_CONTROLLER_SUMMARY_CREATE = "Create Member";
+
+    public static final String MEMBER_CONTROLLER_SUMMARY_DELETE = "Delete Member";
+
+    public static final String MEMBER_CONTROLLER_SUMMARY_UPDATE = "Update Member";
+
+    public static final String MEMBER_CONTROLLER_RESPONSE_200_DESCRIPTION = "Action carried "
+            + "out successfully";
+
+    public static final String MEMBER_CONTROLLER_RESPONSE_201_DESCRIPTION = "Member created";
+
+    public static final String MEMBER_CONTROLLER_RESPONSE_204_DESCRIPTION = "Member deleted";
+
+    public static final String MEMBER_CONTROLLER_RESPONSE_400_DESCRIPTION = "Bad Request";
+
+    public static final String MEMBER_CONTROLLER_RESPONSE_403_DESCRIPTION = "Forbidden, "
+            + "you can not access";
+
+    public static final String MEMBER_CONTROLLER_RESPONSE_404_DESCRIPTION = "Member not found";
+
+    public static final String AUTHENTICATION_CONTROLLER = "Authentication Controller";
+
+    public static final String AUTHENTICATION_CONTROLLER_DESCRIPTION = "Contains the endpoint of Authentication";
+
+    public static final String AUTHENTICATION_CONTROLLER_LOGIN = "User Login";
+
+    public static final String AUTHENTICATION_CONTROLLER_RESPONSE_200_DESCRIPTION = "Action carried out successfully";
+
+    public static final String AUTHENTICATION_CONTROLLER_RESPONSE_400_DESCRIPTION = "Bad Request";
+
+    public static final String AUTHENTICATION_CONTROLLER_RESPONSE_401_DESCRIPTION = "Bad Credentials";
+
+    public static final String AUTHENTICATION_CONTROLLER_RESPONSE_404_DESCRIPTION = "User not found";
+
+    public static final String AUTHENTICATION_CONTROLLER_RESPONSE_500_DESCRIPTION = "Internal Server Error";
+
+    public static final String USER_CONTROLLER = "User Controller";
+
+    public static final String USER_CONTROLLER_DESCRIPTION = "Contains the endpoint of User";
+
+    public static final String USER_CONTROLLER_RESPONSE_200_DESCRIPTION = "Action carried out successfully";
+
+    public static final String USER_CONTROLLER_RESPONSE_400_DESCRIPTION = "Bad Request";
+
+    public static final String USER_CONTROLLER_RESPONSE_403_DESCRIPTION = "Action forbidden";
+
+    public static final String CATEGORY_CONTROLLER = "Category Controller";
+
+    public static final String CATEGORY_CONTROLLER_DESCRIPTION = "Contains the "
+            + "endpoints of category entity";
+
+    public static final String CATEGORY_CONTROLLER_SUMMARY_CREATE = "Create Category";
+
+    public static final String CATEGORY_CONTROLLER_SUMMARY_LIST = "List of Categories";
+
+    public static final String CATEGORY_CONTROLLER_SUMMARY_UPDATE = "Update Category";
+
+    public static final String CATEGORY_CONTROLLER_SUMMARY_DELETE = "Delete Category";
+
+    public static final String CATEGORY_CONTROLLER_RESPONSE_200_DESCRIPTION = "Action carried "
+            + "out successfully";
+
+    public static final String CATEGORY_CONTROLLER_RESPONSE_201_DESCRIPTION = "Category created";
+
+    public static final String CATEGORY_CONTROLLER_RESPONSE_204_DESCRIPTION = "Category deleted";
+
+    public static final String CATEGORY_CONTROLLER_RESPONSE_400_DESCRIPTION = "Bad Request";
+
+    public static final String CATEGORY_CONTROLLER_RESPONSE_403_DESCRIPTION = "Forbidden, "
+            + "you can not access";
+
+    public static final String CATEGORY_CONTROLLER_RESPONSE_404_DESCRIPTION = "Category not found";
+
+
+    public static final String TESTIMONIAL_CONTROLLER = "Testimonial controller";
+
+    public static final String TESTIMONIAL_CONTROLLER_DESCRIPTION = "Contains the endpoints of testimonial entity";
+
+    public static final String TESTIMONIAL_CONTROLLER_SUMMARY_CREATE = "Create testimonial";
+
+    public static final String TESTIMONIAL_CONTROLLER_SUMMARY_LIST = "List of Categories pageds";
+
+    public static final String TESTIMONIAL_CONTROLLER_SUMMARY_DELETE = "Delete testimonial";
+
+    public static final String TESTIMONIAL_CONTROLLER_SUMMARY_UPDATE = "Update testimonial";
+
+    public static final String TESTIMONIAL_CONTROLLER_RESPONSE_201_DESCRIPTION = "Testimonial created";
+
+    public static final String TESTIMONIAL_CONTROLLER_RESPONSE_403_DESCRIPTION = "Forbidden, you can not access";
+
+    public static final String TESTIMONIAL_CONTROLLER_RESPONSE_400_DESCRIPTION = "Bad Request";
+
+    public static final String TESTIMONIAL_CONTROLLER_RESPONSE_200_DESCRIPTION = "Action carried out successfully";
+
+    public static final String TESTIMONIAL_CONTROLLER_RESPONSE_204_DESCRIPTION = "Testimonial deleted";
+
+    public static final String TESTIMONIAL_CONTROLLER_RESPONSE_404_DESCRIPTION = "Testimonial not found";
+}
