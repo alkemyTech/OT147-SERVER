eb4ba8f90dd2916466d0a70b7fcebe5a47625dbe
Pablo Frankenberger <pablo.frankenn@gmail.com>
2022-03-17 14:45:07 -0300

    Added documentation tags to News Controller

--- src/main/java/com/alkemy/ong/controller/NewsController.java
+++ src/main/java/com/alkemy/ong/controller/NewsController.java
@@ -6,8 +6,16 @@ import com.alkemy.ong.exceptions.ParamNotFound;
 import com.alkemy.ong.service.CommentService;
 import com.alkemy.ong.dto.PagesDto;
 import com.alkemy.ong.service.NewsService;
+import com.alkemy.ong.util.DocumentationMessages;
+import com.alkemy.ong.util.DocumentationResponse;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +24,25 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/news")
+@Tag(name = "News", description = "Create, update, show and delete News")
 public class NewsController {
+
     @Autowired
     private NewsService newsService;
+
     @Autowired
     private CommentService commentService;
+
     //get Pageable list of News
+    @Tag(name = "News")
+    @Operation(summary = DocumentationMessages.NEWS_CONTROLLER_SUMMARY_LIST)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationResponse.CODE_OK,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_200_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_BAD_REQUEST,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_400_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_FORBIDDEN,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_403_DESCRIPTION)})
     @GetMapping
     public ResponseEntity<?> getPageNews(@RequestParam(defaultValue = "0") int page){
         PagesDto<NewsDto> response = newsService.getAllPagesNews(page);
@@ -29,19 +50,48 @@ public class NewsController {
     }
 
     //As an Admin see all details in the news entity
+    @Tag(name = "News")
+    @Operation(summary=DocumentationMessages.NEWS_CONTROLLER_SUMMARY_READ )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationResponse.CODE_OK,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_200_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_BAD_REQUEST,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_400_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_FORBIDDEN,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_403_DESCRIPTION)})
     @GetMapping("/{id}")
     public ResponseEntity<NewsDto> getDetailsById(@PathVariable String id) {
         NewsDto newsDto = newsService.getDetailsById(id);
         return ResponseEntity.ok(newsDto);
     }
     //List comments by news id role User
-    @GetMapping("/{id}/comments")
+    @Tag(name = "News")
+    @Operation(summary= DocumentationMessages.NEWS_CONTROLLER_SUMMARY_COMMENTS )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationResponse.CODE_OK,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_200_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_BAD_REQUEST,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_400_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_FORBIDDEN,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_403_DESCRIPTION)})
+    @GetMapping(value="/{id}/comments" ,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<CommentDto>> listCommentsByNewsId(@PathVariable(value = "id") String id){
         List<CommentDto> commentsDtoList = commentService.getCommentsByNewsId(id);
         return ResponseEntity.ok(commentsDtoList);
     }
     //Create news entity only by Admin
-    @PostMapping()
+    @Tag(name = "News")
+    @Operation(summary=DocumentationMessages.NEWS_CONTROLLER_SUMMARY_CREATE )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationResponse.CODE_OK,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_201_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_FORBIDDEN,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_403_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_NOT_FOUND,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_404_DESCRIPTION)})
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<NewsDto> create(@RequestBody @Valid NewsDto dto) {
         try {
             return ResponseEntity.ok(newsService.save(dto));
@@ -50,7 +100,18 @@ public class NewsController {
         }
     }
     //Update information of News only by Admin
-    @PutMapping("/{id}")
+    @Tag(name = "News")
+    @Operation(summary=DocumentationMessages.NEWS_CONTROLLER_SUMMARY_UPDATE )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationResponse.CODE_OK,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_200_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_FORBIDDEN,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_403_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_NOT_FOUND,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_404_DESCRIPTION)})
+    @PutMapping( value ="/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<NewsDto> update(@PathVariable String id, @RequestBody NewsDto dto) {
         try {
             return ResponseEntity.ok(newsService.update(id, dto));
@@ -59,6 +120,15 @@ public class NewsController {
         }
     }
     //Delete News for id  only by Admin
+    @Tag(name = "News")
+    @Operation(summary=DocumentationMessages.NEWS_CONTROLLER_SUMMARY_DELETE  )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = DocumentationResponse.CODE_OK,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_204_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_FORBIDDEN,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_403_DESCRIPTION),
+            @ApiResponse(responseCode = DocumentationResponse.CODE_NOT_FOUND,
+                    description = DocumentationMessages.NEWS_CONTROLLER_RESPONSE_404_DESCRIPTION)})
     @DeleteMapping("/{id}")
     public ResponseEntity<NewsDto> delete(@PathVariable String id){
         try{
