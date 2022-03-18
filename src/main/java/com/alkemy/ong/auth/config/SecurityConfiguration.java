4a6d4feaa5df56de75eb392bdab329bd7f47c3ba
anavduo <50774097+anavduo@users.noreply.github.com>
2022-03-18 11:04:51 -0300

    Revert "Added Documentation tags to Category Controller"

--- src/main/java/com/alkemy/ong/auth/config/SecurityConfiguration.java
+++ src/main/java/com/alkemy/ong/auth/config/SecurityConfiguration.java
@@ -62,11 +62,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(PATCH, "/users/{id}").hasAuthority("USER")
                 .antMatchers(DELETE, "/users/{id}").hasAuthority("USER")
                 //Categories
-                .antMatchers(GET, "/categories").hasAnyAuthority("ADMIN","USER")
+                .antMatchers(GET, "/categories").hasAuthority("ADMIN")
                 .antMatchers(GET,"/categories/{id}").hasAuthority("ADMIN")
-                .antMatchers(POST, "/categories").hasAuthority("ADMIN")
+                .antMatchers(POST, "/categories/create").hasAuthority("ADMIN")
                 .antMatchers(PUT, "/categories/{id}").hasAuthority("ADMIN")
-                .antMatchers(DELETE, "/categories/{id}").hasAuthority("ADMIN")
                 //Organizations
                 .antMatchers(GET, "/organization/public").hasAnyAuthority("ADMIN", "USER")
                 .antMatchers(PUT, "/organization/public/**").hasAuthority("ADMIN")
