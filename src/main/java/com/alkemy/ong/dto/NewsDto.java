f34237fa320b7d3925e9ed29fe50e5ac7ca6680e
Pablo Frankenberger <pablo.frankenn@gmail.com>
2022-03-22 01:02:56 -0300

    Added NewsControllerTest class

--- src/main/java/com/alkemy/ong/dto/NewsDto.java
+++ src/main/java/com/alkemy/ong/dto/NewsDto.java
@@ -4,14 +4,23 @@ import com.alkemy.ong.entity.CategoryEntity;
 import lombok.Data;
 
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 @Data
 public class NewsDto {
     private String id;
+    @NotNull(message = "field name cannot be null")
+    @NotBlank(message = "the name field cannot be empty")
     private String name;
+    @NotNull(message = "field name cannot be null")
+    @NotBlank(message = "the name field cannot be empty")
     private String content;
+    @NotNull(message = "field name cannot be null")
+    @NotBlank(message = "the name field cannot be empty")
     private String image;
     private LocalDateTime timestamps;
+    @NotNull(message = "field name cannot be null")
     private CategoryEntity categoryId;
     private Boolean softDelete;
 }
